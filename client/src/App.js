e6fafa449dcfcb511e663ff1a384ef02afa06a65
jonatan segovia <jsegovia.ush@gmail.com>
2021-09-27 05:48:55 -0300

    react configurated

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,14 @@
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Henry Countries</h1>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <h1>J0n </h1>
+      </div>
+    </BrowserRouter>
   );
 }
 
