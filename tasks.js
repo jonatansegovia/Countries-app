b30cfa8b2ab4fb02e9a43e1006e1195b458d1560
Jonatan Segovia <48600726+jonatansegovia@users.noreply.github.com>
2021-10-07 10:06:17 -0300

    Delete tasks.js

--- tasks.js
+++
@@ -1,8 +0,0 @@
-//ASYNC AWAIT
-//TRY CATCH
-//SEQ INCLUDE TROUGH
-//COUNTRIES IN CREATE STARTS DISORDER
-//DataTypes.UUID, defaultValue: DataTypes.UUIDV4
-//next
-//thunk middleware redux api
-//cambios de promesas a axios fetch
